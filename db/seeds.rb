9f36a078d622ba45366be66a454507abe56e4a8c
Jan Paki <jan.paki@ressourcenmangel.de>
2012-05-23 23:05:45 +0200

    added stages to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,13 @@ User.create!( :email => "yuszuv@gmx.de",
 
 User.create!( :email => "foobar@example.com",
               :password => "foobar" )
+
+puts "creating stages"
+("A".."H").each do |char|
+  Stage.create! name: "Gruppe #{char}"
+end
+Stage.create! name: "Achtelfinale"
+Stage.create! name: "Viertelfinale"
+Stage.create! name: "Halbfinale"
+Stage.create! name: "Spiel um Platz 3"
+Stage.create! name: "Finale"
