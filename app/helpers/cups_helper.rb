2b4969f1f1b1faa49b177c596797698e847c8ded
Jan Paki <jan.paki@ressourcenmangel.de>
2012-05-29 20:30:02 +0200

    routes seems to really work now

--- app/helpers/cups_helper.rb
+++ app/helpers/cups_helper.rb
@@ -4,4 +4,10 @@ module CupsHelper
     return :final_match if match.stage.to_param == "finale"
     match
   end
+
+  def query_caster(path,match)
+    return { stage_id: match.stage.to_param } if path =~ /^\/$|finale/
+    return { group_id: match.stage.to_param } if path =~ /gruppen/
+    {}
+  end
 end
